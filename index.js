c71d7cfe3abbae54dbfa8a1ea3badf719af3864e
Yuferril <yuferril.yunos@gmail.com>
2022-10-05 22:03:00 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// code your solution
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(mondayActivity = "go to the office") {
+  return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(adj='*'){
+    return function (msg){
+        return `You are ${adj}${msg}${adj}!`
+    }
+}
+
+wrapAdjective('||')('a dedicated programmer')
